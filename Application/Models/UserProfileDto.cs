8eff82b161840feed9027e671fcbee8aaef3339f
SAI DUTT <duttganduri46@gmail.com>
2023-06-30 02:36:41 -0700

    initial commit with auth

---
+++ Application/Models/UserProfileDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Application.Models
+{
+	public class UserProfileDto
+	{
+        public string? Id { get; set; }
+        public string? UserName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
+
---
+++ Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string? FirstName { get; set; }
+        [Required]
+        public string? LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
